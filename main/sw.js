2b108cc6c1c142ba635af8d962bf2abe3e988e15
inventorcesarivan <130531517+inventorcesarivan@users.noreply.github.com>
2026-01-08 03:00:28 +0100

    Delete main directory

--- main/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
-});
-// Optional: pass-through fetch keeps it simple
-self.addEventListener('fetch', () => {});
